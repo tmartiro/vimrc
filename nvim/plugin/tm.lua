e1bf36e236670f149fcccca5c652d3b5f67cb99a
Tigran Martirosyan <tmartiro@gmail.com>
2023-03-01 20:43:45 +0400

    golang imports

--- nvim/plugin/tm.lua
+++ nvim/plugin/tm.lua
@@ -5,6 +5,13 @@ vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = '' })
 vim.keymap.set('n', '<leader>e', ':Explore<CR>', { desc = '' })
 vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { desc = '' })
 
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+  end
+})
+
 vim.api.nvim_create_autocmd('BufWritePost', {
   command = 'Format',
   pattern = "*.go",
