04b59f42e7c3580a0ed4f16a1adf4b892b7278a5
Tigran Martirosyan <tmartiro@gmail.com>
2023-03-29 12:44:28 +0400

    switch next and previous buffers by shortcut

--- nvim/plugin/tm.lua
+++ nvim/plugin/tm.lua
@@ -5,6 +5,9 @@ vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = '' })
 vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { desc = '' })
 vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Close the current buffer' })
 vim.keymap.set('n', '<leader>bd!', ':bd!<CR>', { desc = 'Force close the current buffer' })
+vim.keymap.set('n', '<leader><Left>', ':bprevious<CR>', { desc = 'Switch to previous buffer' })
+vim.keymap.set('n', '<leader><Right>', ':bnext<CR>', { desc = 'Switch to next buffer' })
+vim.keymap.set('n', '<leader>bd!', ':bd!<CR>', { desc = 'Force close the current buffer' })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
